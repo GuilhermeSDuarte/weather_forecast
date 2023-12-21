574b8e26eb278e5ac4b66bf675c326ed81e94944
Guilherme Duarte <57077716+GuilhermeSDuarte@users.noreply.github.com>
2023-12-21 20:07:37 -0300

    Previsão de apenas um dia

--- main.py
+++ main.py
@@ -1,46 +1,89 @@
 import requests
+import flet as ft
+from flet import *
 
 
-def main():
+def main(page: ft.Page):
 
-    city_name = input("Informe a cidade:")
+    page.horizontal_alignment = "center"
+    page.vertical_alignment = "center"
 
-    if city_name:
-        menu(city_name)
-    else:
-        print(city_name)
+    #Função para expandir a tela de previsão do dia.
+    def _expand(e):
+        if e.data == "true":
+            _c.content.controls[0].height = 560
+            _c.content.controls[0].update()
+        else:
+            _c.content.controls[0].height = 660 * 0.4
+            _c.content.controls[0].update()
 
+    def _text():
 
-def menu(city_name):
+        text = TextField(width=250,
+                         border_radius=35,
+                         text_align='center',
+                         text_size=15
+                         )
 
+        return text
+
+    #Tela onde vai mostrar a previsão do dia.
+    def _top():
+
+        top = Container(
+            padding=10,
+            width=310,
+            height=660 * 0.44,
+            gradient=LinearGradient(
+                begin=alignment.bottom_left,
+                end=alignment.top_right,
+                colors=["lightblue600", "lightblue900"],
+            ),
+            border_radius=35,
+            animate=animation.Animation(duration=350,
+                                        curve="decelerate"),
+            on_hover=lambda e: _expand(e),
+            content=Column(
+                alignment='start',
+                spacing=10,
+                controls=[
+                    Row(
+                        alignment='center',
+                        controls=[_text()]
+                    )
+                ]
+            ),
+        )
+
+        return top
+
+    #Tela principal do App.
+    _c = Container(
+        width=310,
+        height=660,
+        border_radius=35,
+        bgcolor='black',
+        padding=10,
+        content=Stack(
+            width=210,
+            height=550,
+            controls=[_top()]
+        ),
+
+    )
+
+    page.add(_c)
+
+    """city_name = input("Informe a cidade:")
     api_key = "f785813693eb1ea0af7a78fb3ce3023a"
-    link = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&lang=pt_br"
-    response = requests.get(link)
-    response_dic = response.json()
+    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&lang=pt_br").json()
 
-    opcao = int(input(f"""===Menu===
-    1 - Previsão do tempo atual
-    2 - Previsão do tempo semanal
-    3 - Previsão do tempo de um dia em especifico
-    4 - Dados adicionais de previsão
-    0 - Sair
-    Digite o número do que deseja saber:
-"""))
+    description = response['weather'][0]['description']
+    temperature = response['main']['temp'] - 273.15
 
-    if opcao == 1:
-        description = response_dic['weather'][0]['description']
-        temperature = response_dic['main']['temp'] - 273.15
+    print("Temperatura atual da cidade de", city_name)
+    print("Tempo atual:", description)
+    print("Temperatura atual:", temperature)"""
 
-        print("Temperatura atual da cidade de", city_name)
-        print("Tempo atual:", description)
-        print("Temperatura atual:", temperature)
-    if opcao == 2:
-        pass
-    if opcao == 3:
-        pass
-    if opcao == 4:
-        pass
-    if opcao == 0:
-        pass
 
-main()
+ft.app(target=main)
